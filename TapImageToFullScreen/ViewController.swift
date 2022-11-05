5e10b3aa1cf403306b42ba4b0d17efbef4b1d6b7
sbiljeek <sbiljeekdev@gmail.com>
2022-11-05 16:09:10 +0300

    Initial Commit

---
+++ TapImageToFullScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TapImageToFullScreen
+//
+//  Created by Salman Biljeek on 11/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
